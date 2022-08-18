6c07a4c6ca50032d94a320026fe2d285a36ef2ee
Ivan Alexander Araoz Andrade <69406064+HogRidee@users.noreply.github.com>
2022-08-18 11:31:20 -0500

    Add files via upload

---
+++ Programar en C++/Capitulo2/primerProgramaEnC++/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File  :  main.cpp
+ * Autor :  Ivan Alexander Araoz Andrade
+ * Codigo:  20201216
+ *
+ * Creado el 18 de agosto de 2022, 9:19
+ */
+
+//#include <cstdlib>
+
+#include <iostream>
+
+//sirve para no utilizar std::cout (espacio de nombres std)
+using namespace std;
+
+/*Buena práctica de programación: "Todo programa debe comenzar con un 
+    comentario que describa su propósito"*/
+/*
+ * Capitulo 1: Primer programa en C++
+ */
+int main(int argc, char** argv) {
+    
+    cout <<"Bienvenido a C++ \n";
+    /*El operador << se conoce como el operador de inserción de flujo. Cuando
+     se ejecuta, el valor a la derecha del operador se inserta en el flujo de 
+     salida. */
+    
+    
+    return 0;
+}
+
