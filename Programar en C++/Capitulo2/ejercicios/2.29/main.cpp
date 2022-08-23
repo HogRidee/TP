e7a92c9972a83e201b08d962615534248fd1b5fa
Ivan Alexander Araoz Andrade <69406064+HogRidee@users.noreply.github.com>
2022-08-23 08:00:05 -0500

    Add files via upload

---
+++ Programar en C++/Capitulo2/ejercicios/2.29/main.cpp
@@ -0,0 +1,67 @@
+/* 
+ * File  :  main.cpp
+ * Autor :  Ivan Alexander Araoz Andrade
+ * Codigo:  20201216
+ *
+ * Creado el 22 de agosto de 2022, 12:41
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Este programa responde los ejercicios 2.29
+ */
+int main(int argc, char** argv) {
+    
+    //inicializamos el número a evaluar
+    int numero = 0; 
+    
+    cout << "entero cuadrado cubo" << endl;
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    numero = numero + 1; //podría usarse numero++
+    cout << numero << "\t" << numero * numero << "\t" << numero * numero * numero 
+            << endl;
+    
+    return 0;
+}
+
