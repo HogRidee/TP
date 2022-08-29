da06285a305602c9e51b0ed0c956717561bfef3f
Ivan Alexander Araoz Andrade <69406064+HogRidee@users.noreply.github.com>
2022-08-29 18:04:59 -0500

    Add files via upload

---
+++ Funciones Reutilizables/funcionesMatematicas.hpp
@@ -0,0 +1,26 @@
+/* 
+ * File  :   funcionesMatematicas.hpp
+ * Autor :   Ivan Alexander Araoz Andrade
+ * Codigo:   20201216
+ *
+ * Creado el 23 de agosto de 2022, 7:43
+ */
+
+#ifndef FUNCIONESMATEMATICAS_H
+#define FUNCIONESMATEMATICAS_H
+
+int factorial(int ); //calcula factorial
+bool evaluarCapicua(int ); //evalua un numero, capicua o no
+//calcula digito mayor, menor de un capicua
+void evaluarCapicuaDigitoMayorMenor(int , bool &, int &, int &); 
+
+//NUMEROS COMPLEJOS
+//en c++ ya no se usa *, sino & 
+void sumar(double, double, double, double, double &, double &);
+void restar(double, double, double, double, double &, double &);
+void multiplicar(double, double, double, double, double &, double &);
+void dividir(double, double, double, double, double &, double &, double &);
+void potenciar(double, double, int, double &, double &);
+
+#endif /* FUNCIONESMATEMÁTICAS_H */
+
