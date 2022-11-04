8fb86d6c8b6ee7c86f2c6c1c30c49efae27292a9
Ivan Alexander Araoz Andrade <69406064+HogRidee@users.noreply.github.com>
2022-11-04 12:41:56 -0500

    Update funciones.cpp

--- Semana12/Estructuras/funciones.cpp
+++ Semana12/Estructuras/funciones.cpp
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.cc to edit this template
  */
-#include <valarray>
 
 #include "funciones.hpp"
 #include "smartphone.hpp"
